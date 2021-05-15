be25f053d0c9a1a3b3049af248da589785a9f08f
Victor Barbaros <barbarosvictor@yahoo.com>
2021-05-15 14:06:06 -0400

    Provide project's main structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pypurl
+# Implementation of an easy way of building and handling the Package URLs.
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gym_extensions',
+      version='0.0.1',
+      description='Implementation of an easy way of building and handling the Package URLs.',
+      author='Victor Barbaros',
+      url='https://github.com/vBarbaros/pypurl',
+)
