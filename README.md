a43f2edecf1329658c4fcc5e9d41b504c662cdeb
Victor Barbaros <barbarosvictor@yahoo.com>
2021-05-25 21:28:24 -0400

    Add install and test details to README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Implementation of an easy way of building and handling the Package URLs.
 
+To Install:
+`cd pypurl` and 
+`pip3 install -e .`
+
+Run Tests
+
+`nosetests`
+or 
+`nosetests -v pypurl`
--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+import sys
+from os.path import abspath, join, dirname, realpath
 
-setup(name='gym_extensions',
+def read_requirements_file(filename):
+    req_file_path = '%s/%s' % (dirname(realpath(__file__)), filename)
+    with open(req_file_path) as f:
+        return [line.strip() for line in f]
+setup(name='pypurl',
       version='0.0.1',
+      python_requires='>=3.5',
+      install_requires=read_requirements_file('requirements.txt'),
       description='Implementation of an easy way of building and handling the Package URLs.',
       author='Victor Barbaros',
       url='https://github.com/vBarbaros/pypurl',
